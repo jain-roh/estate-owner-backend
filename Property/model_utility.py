2e7eac9ec9f853e468065c65df93ae45fdfda4e6
Rohit Jain <jain.roh@husky.neu.edu>
2020-11-02 17:20:00 -0500

    Thumbnail size

--- Property/model_utility.py
+++ Property/model_utility.py
@@ -1,13 +1,13 @@
 from io import BytesIO
 from django.core.files import File
 from PIL import Image
-def make_thumbnail(image, size=(300, 200)):
+def make_thumbnail(image, size=(600, 500)):
     """Makes thumbnails of given size from given image"""
 
     im = Image.open(image)
     im.convert('RGB') # convert mode
     im.thumbnail(size) # resize image
     thumb_io = BytesIO() # create a BytesIO object
-    im.save(thumb_io, 'JPEG', quality=85) # save image to BytesIO object
+    im.save(thumb_io, 'JPEG', quality=100) # save image to BytesIO object
     thumbnail = File(thumb_io, name=image.name) # create a django friendly File object
     return thumbnail
