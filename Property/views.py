5f8467c7ea8e930636fd8a3dbf29edb0c48167c3
Rohit Jain <jain.roh@husky.neu.edu>
2020-11-02 23:46:43 -0500

    Images

--- Property/views.py
+++ Property/views.py
@@ -19,7 +19,7 @@ class PropertyView(generics.ListCreateAPIView,UpdateModelMixin):
     serializer_class = PropertySerializer
     queryset = Property.objects.all()
     def post(self, request):
-        file_list=request.FILES.getlist('images')
+        file_list=request.FILES.getlist('images',[])
         print(len(file_list))
         if len(file_list)>0:
             new_file=copy.deepcopy(file_list[0])
