8063aa8f1ac9b51efea2311c548cc9fd5a1babfb
Rohit Jain <jain.roh@husky.neu.edu>
2020-11-20 01:40:44 -0500

    Price Filters

--- Property/filters.py
+++ Property/filters.py
@@ -3,6 +3,19 @@ from .models import Property
 import django_filters
 
 class PropertyFilter(django_filters.FilterSet):
+    price = django_filters.NumberFilter()
+    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
+    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    manufacturer__name = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Property
-        fields = ['address1','address2','city','state', 'price','beds','bath','propertytype' ]
+        fields = {
+            'price':['lte','gte'],
+            'address1':['exact'],
+            'address2':['exact'],
+            'city':['exact'],
+            'state':['exact'],
+            'beds':['gte'],
+            'bath':['gte'],
+            'propertytype':['exact']
+        }
