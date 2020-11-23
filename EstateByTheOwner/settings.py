42a552c1e42b177b645085b3e16330613c8a203f
Rohit Jain <jain.roh@husky.neu.edu>
2020-11-22 22:19:31 -0500

    CHanges to file size

--- EstateByTheOwner/settings.py
+++ EstateByTheOwner/settings.py
@@ -140,7 +140,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
+DATA_UPLOAD_MAX_MEMORY_SIZE = 429916160
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
