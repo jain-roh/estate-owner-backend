7986e741411aab47bbd90b38cb0e388659dcac62
Rohit Jain <jain.roh@husky.neu.edu>
2020-12-18 20:46:58 -0500

    Property Image changes

--- Property/views.py
+++ Property/views.py
@@ -75,12 +75,13 @@ class PropertyView(generics.ListCreateAPIView,UpdateModelMixin):
             prop_img=json.loads(prop_img)
             prop_img_obj=PropertyImages.objects.get(id=prop_img.get('id'))
             print(prop_img['display'])
-            prop_img_serializer=PropertyImageUpdateSerializer(prop_img_obj,dict({'display':prop_img['display']}))
-            print(prop_img_serializer)
+            prop_img_obj.display=prop_img['display']
+            prop_img_serializer=PropertyImageUpdateSerializer(prop_img_obj)
+            # print(prop_img_serializer)
 
             if prop_img_serializer.is_valid():
+            # prop_img_serializer.display
                 prop_img_serializer.save()
-
             else:
                 print(prop_img_serializer.errors)
                 return Response(prop_img_serializer.errors, status=400)
