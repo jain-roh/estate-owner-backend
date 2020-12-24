e8fda97113a44e2e769a663524f9aa9467b8315a
Rohit Jain <jain.roh@husky.neu.edu>
2020-12-24 03:58:46 -0500

    Serializer

--- User/utility.py
+++ User/utility.py
@@ -33,7 +33,7 @@ def create_jwt(request):
         return HttpResponse({'error':'Wrong Username or Password'}, status=401)
     print(type(is_staff))
     print(type(user.is_staff))
-    print(user.is_staff))
+    print(user.is_staff)
     if not user.is_staff==is_staff:
         return HttpResponse({'error': 'Wrong Username or Password or '}, status=401)
     if user.is_staff:
