45ae8115c5fda29fcd196ab39612e8f654abec45
Rohit Jain <rohit.jain@sunfig.c om>
2021-02-19 02:39:08 -0500

    Changesto file

--- User/views.py
+++ User/views.py
@@ -7,14 +7,17 @@ from django.contrib.auth import authenticate
 import jwt
 from rest_framework import status
 import json
+from google.oauth2 import id_token
+
 class UserLogin(generics.ListCreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     def post(self, request):
         data=request.POST
         if data.get('data',None):
-            import requests
+
             if data.get('type')=='facebook':
+                import requests
                 data=json.loads(data.get('data'))
                 response=requests.get('https://graph.facebook.com/'+str(data.get('userID')+'?fields=name,first_name,last_name,email,id&access_token='+str(data.get('accessToken'))))
                 response=json.loads(response.text)
@@ -27,17 +30,27 @@ class UserLogin(generics.ListCreateAPIView):
                 temp_data['password']='Test1234'
                 print(temp_data)
             else:
+                from google.auth.transport import requests
+
                 data = json.loads(data.get('data'))
                 print(data)
                 try:
-                    public_key=requests.get('https://www.googleapis.com/oauth2/v3/certs').text
-                    response=jwt.decode(data.get('code'),public_key)
+                    request = requests.Request()
+
+                    response = id_token.verify_oauth2_token(data['code'],
+                        request, '371591667819-k8q68a5eq9ghrgkrjb5hfj7sjulfetmb.apps.googleusercontent.com')
+
+                    if response['iss'] != 'https://accounts.google.com':
+                        raise ValueError('Wrong issuer.')
+
+                    # public_key=requests.get('https://www.googleapis.com/oauth2/v3/certs').text
+                    # response=jwt.decode(data.get('code'),public_key)
                 except Exception as e:
                     print(e)
                 print(response)
                 temp_data = {}
                 temp_data['first_name'] = response['given_name']
-                temp_data['last_name'] = response['fam ily_name']
+                temp_data['last_name'] = response['family_name']
                 temp_data['username'] = response['sub']
                 temp_data['email'] = response['email']
                 temp_data['is_staff'] = False
