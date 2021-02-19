9aca06e8fe7462e2c57ec75e621ba34bcd886212
Rohit Jain <rohit.jain@sunfig.c om>
2021-02-19 00:44:46 -0500

    Changesto file

--- User/views.py
+++ User/views.py
@@ -15,6 +15,7 @@ class UserLogin(generics.ListCreateAPIView):
             data=json.loads(data.get('data'))
             response=requests.get('https://graph.facebook.com/'+str(data.get('userID')+'?fields=name,first_name,last_name,email,id&access_token='+str(data.get('accessToken'))))
             response=json.loads(response.data)
+            print(response)
             temp_data={}
             temp_data['first_name']=response['first_name']
             print(temp_data)
