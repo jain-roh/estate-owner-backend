0b03e44c7e20be1454b3e5a15bf4bf03104bf7c7
Rohit Jain <rohit.jain@sunfig.c om>
2021-03-01 23:03:24 -0500

    Address filter

--- Property/filters.py
+++ Property/filters.py
@@ -4,15 +4,17 @@ import django_filters
 
 class PropertyFilter(django_filters.FilterSet):
     price = django_filters.NumberFilter()
+    # address1=django_filters.CharFilter()
     price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
     price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
+    address1=django_filters.CharFilter(lookup_expr='icontains')
     manufacturer__name = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Property
         fields = {
             'user':['exact'],
             'price':['lte','gte'],
-            'address1':['exact'],
+            'address1':['contains'],
             'address2':['exact'],
             'city':['exact'],
             'state':['exact'],
